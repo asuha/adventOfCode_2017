11ade9a29d4b272e8454d550e70c0850dc343dbb
Julio Renner <julio.renner@hotmail.com>
2017-12-15 01:07:07 -0200

    day 5

---
+++ day5/day5.py
@@ -0,0 +1,49 @@
+with open('/Users/juliorenner/Documents/git/adventOfCode_2017/day5//input.txt', 'r') as inputFile:
+    input = inputFile.read()
+
+    # list = [0, 3,0, 1, -3]
+
+    def parte1(): 
+        inputList = input.split("\n")
+        index = 0
+
+        stepsCount = 0
+        while True:
+            previous  = index
+
+            steps = int(inputList[index])
+            stepsCount += 1
+
+            if len(inputList) > (index + steps) and -1 < (index + steps) :
+                inputList[previous] = int(inputList[previous])
+                inputList[previous] = inputList[previous] + 1
+                index = index + steps
+            else:
+                break;
+        return stepsCount   
+
+    def parte2():
+        inputList = input.split("\n")
+        index = 0
+
+        stepsCount = 0
+        while True:
+            previous  = index
+
+            steps = int(inputList[index])
+            stepsCount += 1
+
+            if len(inputList) > (index + steps) and -1 < (index + steps) :
+                inputList[previous] = int(inputList[previous])
+                if inputList[previous] > 2:
+                    inputList[previous] = inputList[previous] - 1
+                else:
+                    inputList[previous] = inputList[previous] + 1
+                index = index + steps
+            else:
+                break;
+        
+        return stepsCount
+        
+    print('Parte 1: ' + str(parte1()))
+    print('Parte 2: ' + str(parte2()))
